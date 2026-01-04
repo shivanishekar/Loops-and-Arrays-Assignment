8acd93c1c79e6071189d226b6fb3a42b17c37c75
shivanishekar <shivagamishivani06@gmail.com>
2026-01-04 18:39:04 +0530

    Create String.java

---
+++ String.java
@@ -0,0 +1,17 @@
+// 1. Write a program to create String array and print all the values in that array.
+public class StringArrayExample {
+
+    public static void main(String[] args) {
+
+        String[] names = {"Ram", "Sita", "Lakshman", "Hanuman"};
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+    }
+}
+/* output : 
+Ram
+Sita
+Lakshman
+Hanuman */
